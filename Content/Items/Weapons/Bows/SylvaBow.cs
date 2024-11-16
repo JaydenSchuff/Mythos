e840b1de06f1a64593be039e813aff4ad7ebd212
Jayden <jdrich135@gmail.com>
2024-11-15 23:05:51 -0700

    Added Sylva Bow and Sprite along with new Bow folder

---
+++ Content/Items/Weapons/Bows/SylvaBow.cs
@@ -0,0 +1,42 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Mythos.Content.Projectiles.Weapons;
+
+namespace Mythos.Content.Items.Weapons.Bows
+{ 
+    public class SylvaBow : ModItem
+    {
+        public override void SetDefaults()
+        {
+           Item.width = 40;
+           Item.height = 80;
+           Item.scale = 0.8f;
+
+           Item.useTime = 32;
+           Item.useAnimation = 32;
+
+           Item.useStyle = ItemUseStyleID.Shoot;
+
+           Item.autoReuse = true;
+
+           Item.UseSound = SoundID.Item5;
+
+           Item.DamageType = DamageClass.Ranged;
+           Item.damage = 75;
+           Item.knockBack = 3f;
+           Item.noMelee = true;
+
+           Item.shoot = ProjectileID.WoodenArrowFriendly;
+           Item.shootSpeed = 12f;
+
+           Item.useAmmo = AmmoID.Arrow;
+        }
+
+        public override Vector2? HoldoutOffset()
+        {
+            return new Vector2(-4f, 2f);
+        }
+    }
+}
